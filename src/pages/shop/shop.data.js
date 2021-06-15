efdfd3c5c3dbeebf98fbe92ee8e442a3f386e75a
naderrazavi87 <nader.razavi87@gmail.com>
2021-06-14 23:53:10 -0700

    update shop.data with new data , updated cart utils to group items in cart reducer

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -246,6 +246,4 @@ const SHOP_DATA = [
   }
 ];
 
-
-export default SHOP_DATA
-
+export default SHOP_DATA;
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,28 +1,9 @@
-// import CartActionTypes from './cart.types';
-
-
-// const INITIAL_STATE	={
-// 	hidden: true
-// }
-
-// const cartReducer=(state=INITIAL_STATE, action) =>{
-// 	switch(action.type){
-// 		case CartActionTypes.TOGGLE_CART_HIDDEN:
-// 			return {
-// 				...state,
-// 				hidden: !state.hidden
-// 		}
-// 		default:
-// 			return state;
-// 	}
-
-// }
-
-// export default cartReducer;
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -31,6 +12,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
       };
     default:
       return state;
